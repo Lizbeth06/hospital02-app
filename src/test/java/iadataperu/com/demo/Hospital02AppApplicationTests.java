23709b0f974d986ed820ef396060e03a874a7ed9
Lizbeth06 <lizbeth.infante.27@unsch.edu.pe>
2024-08-15 10:25:36 -0500

    primer commit

---
+++ src/test/java/iadataperu/com/demo/Hospital02AppApplicationTests.java
@@ -0,0 +1,13 @@
+package iadataperu.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hospital02AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
